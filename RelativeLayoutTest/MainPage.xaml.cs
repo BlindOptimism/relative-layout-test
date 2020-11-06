b2e2567d4b1c3d8e6236a2c5cf34b6a9f4eae9f9
BlindOptimism <warren.kuyk@gmail.com>
2020-11-06 09:00:51 +0200

    first commit

---
+++ RelativeLayoutTest/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RelativeLayoutTest
+{
+    public partial class MainPage : ContentPage
+    {
+        double _credit = 11.95;
+        public MainPage()
+        {
+            InitializeComponent();
+            totalCredit.Text = $"${_credit}";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _credit += 8.00;
+            totalCredit.Text = $"${_credit}";
+        }
+    }
+}
